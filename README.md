0dd91ebf0aad7fb36ac4f1626fa17e530c849f66
notplus <notplus@163.com>
2021-03-19 21:13:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Satellite Coordinate Compute
+
+
+
+## Demo Data format
+
+brdc\*\*\*\*\.\*\*n
+为IGS提供的GPS广播星历文件，文件中只包括GPS的星历数据。
+
+brdm\*\*\*\*.\*\*p
+为IGS提供的多系统广播星历文件，文件中包括GNSS四系统和部分其他系统的星历数据。
+
+igs\*\*\*\*\*.sp3
+为IGS提供的GPS精密星历文件。
+
+gbm\*\*\*\*\*.sp3
+为gbm提供的多系统精密星历文件。
+
---
+++ compute.py
@@ -0,0 +1,42 @@
+'''
+Description: 
+Author: notplus
+Date: 2021-03-19 20:56:10
+LastEditors: notplus
+LastEditTime: 2021-03-19 21:09:32
+'''
+
+import numpy as np
+import os
+import argparse
+import sys
+
+
+def parse_args():
+    """
+       Parse input arguments
+       """
+    parser = argparse.ArgumentParser(
+        description='Compute satellite coordinate')
+    parser.add_argument('--e', dest='ephemeris',
+                        help='input the ephemeris file',
+                        default=None, type=str)
+    parser.add_argument('--o', dest='out_path',
+                        help='output file path',
+                        default=None, type=str)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    print('Called with args:')
+    print(args)
+
+    

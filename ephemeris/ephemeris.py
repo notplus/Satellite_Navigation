bf534a41e3bf656ffe9d5fb9d44fb592def296ca
notplus <notplus@163.com>
2021-03-22 08:03:59 +0800

    refactor: use readlines to parse file; fix: empty line bug

--- ephemeris/ephemeris.py
+++ ephemeris/ephemeris.py
@@ -3,7 +3,7 @@ Description:
 Author: notplus
 Date: 2021-03-19 21:35:25
 LastEditors: notplus
-LastEditTime: 2021-03-21 22:07:30
+LastEditTime: 2021-03-22 08:03:04
 '''
 
 from utils.constant import *
@@ -78,88 +78,61 @@ class Ephemeris:
     def __parseFile(self, path):
         print("parse file %s" % path)
         with open(path, 'r') as f:
+            lines = f.readlines()
+
             # RINEX 2 GPS Navigation
             # parse header
             # line 1 RINEX VERSION / TYPE
-            self.__rinex_version = f.read(9)
-            f.read(11)
-            self.__type = f.read(1)
-            f.read(19)
-            f.read(40)
-            f.read(1)
+            self.__rinex_version = lines[0][0:9]
+            self.__type = lines[0][20:21]
 
             # line 2 PGM / RUN BY / DATE
-            self.__pgm = f.read(20)
-            self.__run_by = f.read(20)
-            self.__data = f.read(20)
-            f.read(20)
-            f.read(1)
+            self.__pgm = lines[1][0:20]
+            self.__run_by = lines[1][20:40]
+            self.__data = lines[1][40:60]
 
             # line 3 COMMENT
-            self.__comment = f.read(60)
-            f.read(20)
-            f.read(1)
-
+            self.__comment = lines[2][0:60]
+            
             # line 4 ION ALPHA
-            f.read(2)
-            self.__ion_alpha_a0 = parseDouble(f.read(12))
-            self.__ion_alpha_a1 = parseDouble(f.read(12))
-            self.__ion_alpha_a2 = parseDouble(f.read(12))
-            self.__ion_alpha_a3 = parseDouble(f.read(12))
-            f.read(30)
-            f.read(1)
+            self.__ion_alpha_a0 = parseDouble(lines[3][2:14])
+            self.__ion_alpha_a1 = parseDouble(lines[3][14:26])
+            self.__ion_alpha_a2 = parseDouble(lines[3][26:40])
+            self.__ion_alpha_a3 = parseDouble(lines[3][40:52])
 
             # line 5 ION BETA
-            f.read(2)
-            self.__ion_beta_b0 = parseDouble(f.read(12))
-            self.__ion_beta_b1 = parseDouble(f.read(12))
-            self.__ion_beta_b2 = parseDouble(f.read(12))
-            self.__ion_beta_b3 = parseDouble(f.read(12))
-            f.read(30)
-            f.read(1)
+            self.__ion_beta_b0 = parseDouble(lines[4][2:14])
+            self.__ion_beta_b1 = parseDouble(lines[4][14:26])
+            self.__ion_beta_b2 = parseDouble(lines[4][26:40])
+            self.__ion_beta_b3 = parseDouble(lines[4][40:52])
 
             # line 6 DELTA-UTC: A0,A1,T,W
-            f.read(3)
-            self.__a0 = parseDouble(f.read(19))
-            self.__a1 = parseDouble(f.read(19))
-            self.__t = int(f.read(9))
-            self.__w = int(f.read(9))
-            f.read(21)
-            f.read(1)
+            self.__a0 = parseDouble(lines[5][3:22])
+            self.__a1 = parseDouble(lines[5][22:41])
+            self.__t = int(lines[5][41:50])
+            self.__w = int(lines[5][50:59])
 
             # line 7 LEAP SECONDS
-            self.__delta_t = int(f.read(6))
-            f.read(74)
-            f.read(1)
+            self.__delta_t = int(lines[6][0:6])
 
             # line 8 END OF HEADER
-            f.read(60)
-            f.read(20)
-            f.read(1)
 
             # satellite records
-            self.__records=[]
-            while f:
+
+            self.__records = []
+            for i in range(8,len(lines),8):
                 # line 1
-                prn = int(f.read(2))
-                f.read(1)
-                year = int(f.read(2))
+                prn = int(lines[i][0:2])
+                year = int(lines[i][3:5])
+                month = int(lines[i][6:8])
+                day = int(lines[i][9:11])
+                hour = int(lines[i][12:14])
+                minute = int(lines[i][15:17])
+                second = float(lines[i][17:22])
 
-                f.read(1)
-                month = int(f.read(2))
-                f.read(1)
-                day = int(f.read(2))
-                f.read(1)
-                hour = int(f.read(2))
-                f.read(1)
-                minute = int(f.read(2))
-
-                second = float(f.read(5))
-
-                clock_bias = parseDouble(f.read(19))
-                clock_drift = parseDouble(f.read(19))
-                clock_drift_rate = parseDouble(f.read(19))
-                f.read(1)
+                clock_bias = parseDouble(lines[i][22:41])
+                clock_drift = parseDouble(lines[i][41:60])
+                clock_drift_rate = parseDouble(lines[i][60:79])
 
                 oc = OC(year, month, day, hour, minute, second)
 
@@ -167,49 +140,35 @@ class Ephemeris:
                     prn, oc, clock_bias, clock_drift, clock_drift_rate)
 
                 # Orbit 1
-                f.read(3)
-                record.setOrbit1(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
-                
+                record.setOrbit1(parseDouble(lines[i+1][3:22]), parseDouble(lines[i+1][22:41]),
+                                 parseDouble(lines[i+1][41:60]), parseDouble(lines[i+1][60:79]))
+
                 # Orbit 2
-                f.read(3)
-                record.setOrbit2(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
+                record.setOrbit2(parseDouble(lines[i+2][3:22]), parseDouble(lines[i+2][22:41]),
+                                 parseDouble(lines[i+2][41:60]), parseDouble(lines[i+2][60:79]))
 
                 # Orbit 3
-                f.read(3)
-                record.setOrbit3(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
+                record.setOrbit3(parseDouble(lines[i+3][3:22]), parseDouble(lines[i+3][22:41]),
+                                 parseDouble(lines[i+3][41:60]), parseDouble(lines[i+3][60:79]))
 
                 # Orbit 4
-                f.read(3)
-                record.setOrbit4(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
+                record.setOrbit4(parseDouble(lines[i+4][3:22]), parseDouble(lines[i+4][22:41]),
+                                 parseDouble(lines[i+4][41:60]), parseDouble(lines[i+4][60:79]))
 
                 # Orbit 5
-                f.read(3)
-                record.setOrbit5(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
+                record.setOrbit5(parseDouble(lines[i+5][3:22]), parseDouble(lines[i+5][22:41]),
+                                 parseDouble(lines[i+5][41:60]), parseDouble(lines[i+6][60:79]))
 
                 # Orbit 6
-                f.read(3)
-                record.setOrbit6(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
+                record.setOrbit6(parseDouble(lines[i+6][3:22]), parseDouble(lines[i+6][22:41]),
+                                 parseDouble(lines[i+6][41:60]), parseDouble(lines[i+6][60:79]))
 
                 # Orbit 7
-                f.read(3)
-                record.setOrbit7(parseDouble(f.read(19)), parseDouble(f.read(19)),
-                                 parseDouble(f.read(19)), parseDouble(f.read(19)))
-                f.read(1)
-                
+                record.setOrbit7(parseDouble(lines[i+7][3:22]), parseDouble(lines[i+7][22:41]),
+                                 parseDouble(lines[i+7][41:60]), parseDouble(lines[i+7][60:79]))
+
                 self.__records += [record]
 
     def test(self):
         print(self.__rinex_version)
-        
+        print(len(self.__records))
